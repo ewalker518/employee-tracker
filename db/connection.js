a1bf7cd13ef7e7d45eca7d20dce392a1ffd7821b
saltyposaune <ewalkertbone@gmail.com>
2021-08-28 18:57:57 -0600

    fixed dependencies

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-// const db = mysql.createConnection(
-//     {
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//         database: 'election'
-//     },
-//     console.log('Connected to the election database.')
-// );
+require('dotenv').config();
 
-// module.exports = db;
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'employees'
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const express = require('express')
 const db = require('./db/connection');
 const PORT = process.env.PORT || 3002;
 const app = express();
-require('dotenv').config()
+const chalk = require('chalk');
+const figlet = require('figlet');
+// const routes = require('./routes');
+
+db.connect((error) => {
+    if (error) throw error;
+    console.log(chalk.red.bold(`====================================================================================`));       // https://www.npmjs.com/package/chalk
+    console.log(``);
+    console.log(chalk.yellowBright.bold(figlet.textSync('Employee Tracker')));                                                 // http://www.figlet.org/
+    console.log(``);
+    console.log(chalk.red.bold(`====================================================================================`));
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 // Start server after DB connection
 app.listen(PORT, () => {
